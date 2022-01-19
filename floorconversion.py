2dcaacc17f2337c3ea2c17a368806c58f4f9e3da
Jimmijangas <98039089+Jimmijangas@users.noreply.github.com>
2022-01-19 09:54:56 -0600

    Update floorconversion.py

--- floorconversion.py
+++ floorconversion.py
@@ -1,4 +1,4 @@
 print('EU to US Floor Conversion')
-euf = input('What is the European floor number?')
+euf = input('What is the European floor number? ')
 usf = int(euf) + 1
 print('Your US floor number is', usf)
